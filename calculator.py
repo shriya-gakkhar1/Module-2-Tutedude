2a7482285cf1656d5f0fb2e35ad08c9f66f52ff5
shriyagakkhar1 <sgakkhar2005@gmail.com>
2025-03-22 14:43:50 +0530

    Assignment 2

---
+++ calculator.py
@@ -0,0 +1,19 @@
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+if num2 != 0:
+    division = num1 / num2
+else:
+    division = "undefined (division by zero)"
+
+# Display the results
+print(f"Addition:  {addition}")
+print(f"Subtraction:  {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
